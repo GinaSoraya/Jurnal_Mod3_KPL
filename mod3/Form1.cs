57af182a8c589a7cc62e16e659b76b7a6e53fac9
GinaSoraya <ginasry08@gmail.com>
2024-03-04 08:28:08 +0700

    menambahkan implementasi kalkulator

---
+++ mod3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mod3
+{
+    public partial class Form1 : Form
+    {
+        private int angka;
+        private int nomor;
+        private string oprasi;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+            nomor = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+            nomor = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+            nomor = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+            nomor = 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+            nomor = 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+            nomor = 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+            nomor = 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+            nomor = 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+            nomor = 9;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            oprasi = "+";
+            angka = Convert.ToInt32(textBox1);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+            nomor = 0;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            nomor = Convert.ToInt32(textBox1);
+            if (oprasi == "+")
+            {
+                textBox1 = Convert.ToString(angka + nomor);
+            }
+            textBox1.Show();
+        }
+    }
+}
